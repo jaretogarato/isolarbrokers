b302e10c56378c9243cfa2255dacfbdd4527c868
Jared Gallardo <jared@star.solar>
2020-07-02 15:35:23 -0600

    cleanup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,24 +8,15 @@ import AccountNew from './AccountNew';
 import Flash from './Flash';
 import Home from './Home';
 import OurPromise from './OurPromise';
-import Token from './Token';
 import Contact from './Contact';
-import Wallet from './Wallet';
-import Redeem from './Redeem';
-import Purchase from './Purchase';
 import Resources from './Resources';
 import Success from './Success';
 import RegistrationPending from './RegistrationPending';
-import Profile from './Profile';
-import USMint from './USMint';
-import Fincen from './Fincen';
-// import Terms from './Terms';
 import Privacy from './Privacy';
 import Dashboard from './Dashboard';
 import Leads from './Leads';
 import Users from './Users';
 import UserAgreement from './UserAgreement';
-import TokenPurchaseAgreement from './TokenPurchaseAgreement';
 // import FormWrap from './FormValidationEg';
 import ReactTrackDemo from './ReactTrackDemo';
 import ProtectedRoute from './ProtectedRoute';
@@ -46,29 +37,17 @@ class App extends Component {
           <Switch>
             <Route exact path='/' component={Home} />
             <Route path='/our-promise' component={OurPromise} />
-            <Route path='/token' component={Token} />
             <Route path='/contact' component={Contact} />
-            <Route path='/wallet' component={Wallet} />
-            <Route path='/redeem' component={Redeem} />
-            <Route path='/purchase' component={Purchase} />
             <Route path='/resources' component={Resources} />
             <Route path='/success' component={Success} />
-            <Route path='/usmint' component={USMint} />
-            <Route path='/fincen' component={Fincen} />
             <Route path='/privacy' component={Privacy} />
-            <Route path='/token-purchase-agreement' component={TokenPurchaseAgreement} />
             <Route path='/user-agreement' component={UserAgreement} />
             {/* <Route path='/form-validation-eg' component={FormWrap} /> */}
-
-
             <Route exact path='/register' component={AccountNew} />
             <Route exact path='/registration-pending' component={RegistrationPending} />
-
             <AuthRoute exact path='/login' component={Login} />
-            {/* <Route exact path='/register2' component={AccountNew} /> */}
             <ProtectedRoute path='/leads' component={Leads} />
             <ProtectedRoute path='/users' component={Users} />
-            <ProtectedRoute path='/profile/*' component={Profile} />
             <ProtectedRoute path='/dashboard/*' component={Dashboard} />
             <Route component={NoMatch} />
           </Switch>
